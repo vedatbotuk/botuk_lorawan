c884c3058bb21f1cda348621c584d4890873a4be
Vedat <vedat.botuk@gmail.com>
2024-09-13 08:59:29 +0200

    add credentiels_sample.h

---
+++ credentials_sample.h
@@ -0,0 +1,83 @@
+// credentials.h
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Definitions for devEui
+#define DEV_EUI_0 0xXX
+#define DEV_EUI_1 0xXX
+#define DEV_EUI_2 0xXX
+#define DEV_EUI_3 0xXX
+#define DEV_EUI_4 0xXX
+#define DEV_EUI_5 0xXX
+#define DEV_EUI_6 0xXX
+#define DEV_EUI_7 0xXX
+
+// Definitions for appEui
+#define APP_EUI_0 0xXX
+#define APP_EUI_1 0xXX
+#define APP_EUI_2 0xXX
+#define APP_EUI_3 0xXX
+#define APP_EUI_4 0xXX
+#define APP_EUI_5 0xXX
+#define APP_EUI_6 0xXX
+#define APP_EUI_7 0xXX
+
+// Definitions for appKey
+#define APP_KEY_0  0xXX
+#define APP_KEY_1  0xXX
+#define APP_KEY_2  0xXX
+#define APP_KEY_3  0xXX
+#define APP_KEY_4  0xXX
+#define APP_KEY_5  0xXX
+#define APP_KEY_6  0xXX
+#define APP_KEY_7  0xXX
+#define APP_KEY_8  0xXX
+#define APP_KEY_9  0xXX
+#define APP_KEY_10 0xXX
+#define APP_KEY_11 0xXX
+#define APP_KEY_12 0xXX
+#define APP_KEY_13 0xXX
+#define APP_KEY_14 0xXX
+#define APP_KEY_15 0xXX
+
+// Definitions for nwkSKey
+#define NWK_SKEY_0 0xXX
+#define NWK_SKEY_1 0xXX
+#define NWK_SKEY_2 0xXX
+#define NWK_SKEY_3 0xXX
+#define NWK_SKEY_4 0xXX
+#define NWK_SKEY_5 0xXX
+#define NWK_SKEY_6 0xXX
+#define NWK_SKEY_7 0xXX
+#define NWK_SKEY_8 0xXX
+#define NWK_SKEY_9 0xXX
+#define NWK_SKEY_10 0xXX
+#define NWK_SKEY_11 0xXX
+#define NWK_SKEY_12 0xXX
+#define NWK_SKEY_13 0xXX
+#define NWK_SKEY_14 0xXX
+#define NWK_SKEY_15 0xXX
+
+// Definitions for appSKey
+#define APP_SKEY_0 0xXX
+#define APP_SKEY_1 0xXX
+#define APP_SKEY_2 0xXX
+#define APP_SKEY_3 0xXX
+#define APP_SKEY_4 0xXX
+#define APP_SKEY_5 0xXX
+#define APP_SKEY_6 0xXX
+#define APP_SKEY_7 0xXX
+#define APP_SKEY_8 0xXX
+#define APP_SKEY_9 0xXX
+#define APP_SKEY_10 0xXX
+#define APP_SKEY_11 0xXX
+#define APP_SKEY_12 0xXX
+#define APP_SKEY_13 0xXX
+#define APP_SKEY_14 0xXX
+#define APP_SKEY_15 0xXX
+
+// Definitions for devAddr
+#define DEV_ADDR 0xXXXXXXXX
+
+#endif
+
